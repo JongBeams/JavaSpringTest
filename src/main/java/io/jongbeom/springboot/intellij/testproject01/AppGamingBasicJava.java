f6d2f716869648b956f89b911cefdcd837127dcc
JongBeams <wwhow2003@gmail.com>
2025-10-30 14:14:28 +0900

    testcommit

---
+++ src/main/java/io/jongbeom/springboot/intellij/testproject01/AppGamingBasicJava.java
@@ -0,0 +1,13 @@
+package io.jongbeom.springboot.intellij.testproject01;
+
+import org.springframework.boot.SpringApplication;
+
+public class AppGamingBasicJava {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testproject01Application.class, args);
+
+
+
+    }
+}
